b8eb59a8d0fda14f8c285e7fcff32728a1c1d083
smh1988-2 <seamhurley1988@gmail.com>
2021-11-02 17:08:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// function mondayWork (activity = "go to the office") {
+//     return `This Monday, I will ${activity}.`;
+// }
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(wrap = "*") {
+    return function (adj = "special") {
+        return `You are ${wrap}${adj}${wrap}!`;
+    }
+    
+}
+
+wrapAdjective("*")("a hard worker");
+wrapAdjective("|||")("a dedicated programmer");
